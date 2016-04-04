7444b6a5e3d9bb857079c76ab53b8b2f7ba39832
System Administrator <root@STANDARD.local>
2016-04-04 14:56:14 -0400

    add circle push project dir

---
+++ projects/push/sketch.js
@@ -0,0 +1,94 @@
+var bX = 999;
+var bY = 666;
+var bRadius = 222;
+
+var cX = 1111;
+var cY = 444;
+var cRadius = 222;
+
+var dX = 555;
+var dY = 333;
+var dRadius = 222;
+
+var diagBuf = 111; //diagonal buffer to allow to push just up, or just left...
+var counter = 0;
+
+
+function setup() {
+
+}
+
+function draw() {
+  createCanvas(windowWidth, windowHeight);
+  background(246, 247, 235);
+  noStroke();
+
+  fill(63, 136, 197);
+  text(counter, 10, 10);
+
+  //BLUE CIRCLE
+  fill(63, 136, 197);
+  ellipse(dX, dY, cRadius, cRadius);
+  textAlign(CENTER,CENTER);
+  fill(255,255,255);
+  textSize(dRadius/5);
+  text("Grab Me",dX,dY);
+  if (mouseIsPressed && mouseX > dX - 50 && mouseX < dX + 50 && mouseY > dY - 50 && mouseY < dY + 50) {
+    dX = mouseX;
+    dY = mouseY;
+  }
+
+
+  //RED CIRCLE
+  fill(233, 78, 55);
+  ellipse(cX, cY, cRadius, cRadius);
+
+  ///push left
+  if (dX >= cX + 11 && dX < cX + cRadius / 1.33 && dY >= cY - cRadius / 1.33 && dY <= cY + cRadius / 1.33) {
+    cX = cX - 4;
+  }
+  ///push right
+  if (dX <= cX - 11 && dX > cX - cRadius / 1.33 && dY >= cY - cRadius / 1.33 && dY <= cY + cRadius / 1.33) {
+    cX = cX + 4;
+  }
+  ///push up
+  if (dX <= cX + cRadius / 1.33 && dX > cX - cRadius / 1.33 && dY <= cY + cRadius / 1.33 && dY >= cY + 11) {
+    cY = cY - 4;
+  }
+  ///push down
+  if (dX <= cX + cRadius / 1.33 && dX >= cX - cRadius / 1.33 && dY <= cY - 11 && dY >= cY - cRadius / 1.33) {
+    cY = cY + 4;
+  }
+  fill(255,255,255);
+  text("Push Me",cX,cY);
+
+
+  ///GREEN Circle 'b'
+  fill(68, 187, 164);
+  ellipse(bX, bY, bRadius, bRadius);
+
+  ///push left
+  if (dX >= bX + 11 && dX < bX + dRadius / 1.33 && dY >= bY - dRadius / 1.33 && dY <= bY + dRadius / 1.33) {
+    bX = bX - 5;
+  }
+  ///push right
+  if (dX <= bX - 11 && dX > bX - bRadius / 1.33 && dY >= bY - bRadius / 1.33 && dY <= bY + bRadius / 1.33) {
+    bX = bX + 5;
+  }
+  ///push up
+  if (dX <= bX + bRadius / 1.33 && dX > bX - bRadius / 1.33 && dY <= bY + bRadius / 1.33 && dY >= bY + 11) {
+    bY = bY - 5;
+  }
+  ///push down
+  if (dX <= bX + bRadius / 1.33 && dX >= bX - bRadius / 1.33 && dY <= bY - 11 && dY >= bY - bRadius / 1.33) {
+    bY = bY + 5;
+  }
+  fill(255,255,255);
+  text("Push Me",bX,bY);
+}
+
+function mouseReleased() {
+  counter = counter + 1;
+  fill(111);
+  rect(bX, bY, 2222, 2222);
+}
