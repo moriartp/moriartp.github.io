72087c20ae9490fcf988d67da6108efdb409c78c
System Administrator <root@w-900f-14966.tns.newschool.edu>
2016-10-17 12:54:55 -0400

    coloring icons

--- projects/d3scratch/script.js
+++ projects/d3scratch/script.js
@@ -14,63 +14,51 @@ var data = d3.csv("dummyRoomDataX.csv", function(error, data) {
     .data(data).enter()
     // .append('div.tile')
     .append('div')
-      .attr('class', "tile")//function (d) { return d.class; })
-      // .attr('class', function (d) { return d.type; })
-      // .attr('data-filter', function (d) { return d.type; })
-      // .attr('class', function (d) { return d.platform; })
-      .style('float','left')
-      .style('padding','5px')
-      .style('margin','2px')
-      .style('width', "200px")
-      .style('height', "225px")
-      .style('background-color', "WhiteSmoke")//function (d) { return d.backgroundColor; })
-      .style('border',"2px solid #F9F9F7")
-
+      .attr('class', "tile")
       .on('mouseover', function() {
         d3.select(this)
         .transition().duration(100)
-        .style('background-color','#E82E21')
-        // .style("width","1250px")
+        .style('background-color','#F6594D')//'#E82E21')
+        // .style("height","666px")
       })
 
       .on('mouseout', function () {
         d3.select(this)
         .transition().duration(500)
         .style('background-color', 'WhiteSmoke')//function (d) { return d.backgroundColor; })
-        // .style("width","150px")
+        // .style("height","333px")
       })
 
       .append('text')
       // .html(function (d) { return "<font size='5'>"+d.roomID+"</font><br>"+d.type+"<br>"+d.location+"<br>Macs: "+d.macQty+"<br>Wins: "+d.winQty; });
       .html(function (d) { return "<font size='5'>"+d.roomID+"</font><br>"+d.type+"<br>"+d.location+"; Rm. "+d.roomID+"<br>"; });   
-      
 
-      d3.selectAll("div").append("macs")
+      d3.selectAll("text").append("mac")
         // .text(function (d) { return d.macQty})
       .filter( function(d) {return d.macQty > 0; })
         .html(function (d) { return "<br><i class='fa fa-apple' fa-lg></i> Mac Stations: "+d.macQty; })
 
-      d3.selectAll("div").append("wins")
+      d3.selectAll("text").append("wins")
       .filter( function(d) {return d.winQty > 0; })
         .html(function (d) { return "<br><i class='fa fa-windows' fa-lg></i> Windows Stations: "+d.winQty})
 
-      d3.selectAll("div").append("projector")
+      d3.selectAll("text").append("projector")
       .filter( function(d) {return d.Projection === 'TRUE'; })
         .html(function (d) { return "<br><i class='fa fa-video-camera' fa-lg></i> Projector" })
 
-      d3.selectAll("div").append("scan")
+      d3.selectAll("text").append("scan")
       .filter( function(d) {return d.scanQty > 0; })
         .html(function (d) { return "<br><i class='fa fa-clipboard' fa-lg></i> Scanners: "+d.scanQty; })
 
-      d3.selectAll("div").append("dvd")
+      d3.selectAll("text").append("dvd")
       .filter( function(d) {return d.DVD === 'TRUE'; })
         .html(function (d) { return "<br><i class='fa fa-play-circle-o' fa-lg></i> DVD Playback" })
 
-      d3.selectAll("div").append("vhs")
+      d3.selectAll("text").append("vhs")
       .filter( function(d) {return d.VHS === 'TRUE'; })
         .html(function (d) { return "<br><i class='fa fa-film' fa-lg></i> VHS Playback" })
 
-      d3.selectAll("div").append("cd")
+      d3.selectAll("text").append("cd")
       .filter( function(d) {return d.CD === 'TRUE'; })
         .html(function (d) { return "<br><i class='fa fa-music' fa-lg></i> CD Playback" })
 
@@ -125,14 +113,14 @@ var data = d3.csv("dummyRoomDataX.csv", function(error, data) {
       tooltip.html('').html('<b>'+d.roomID+'</b><br>Location: '+d.location)
 
 
-      // ////Calculate positioning and move tooltip THIS IS THE ORIGINAL
-      var ttBCR = tooltip.node().getBoundingClientRect()
-      var topPosition = mouseY - ttBCR.height + pageYOffset - 14
-      var leftPosition = ( mouseX - ttBCR.width*1 ) + pageXOffset
+      // // ////Calculate positioning and move tooltip THIS IS THE ORIGINAL
+      // var ttBCR = tooltip.node().getBoundingClientRect()
+      // var topPosition = mouseY - ttBCR.height + pageYOffset - 14
+      // var leftPosition = ( mouseX - ttBCR.width*1 ) + pageXOffset
       
       ////Calculate positioning and move tooltip
       var ttBCR = tooltip.node().getBoundingClientRect()
-      var topPosition = mouseY - ttBCR.height + pageYOffset - 14
+      var topPosition = mouseY - ttBCR.height + pageYOffset + 200
       var leftPosition = ( mouseX - ttBCR.width*1 ) + pageXOffset + 100
 
 
