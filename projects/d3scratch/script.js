f1d31af6ed670d2c566c5b158579e4e30cf7b57e
System Administrator <root@w-900f-14966.tns.newschool.edu>
2016-11-07 16:53:40 -0500

    tooltip fixed position attr

--- projects/d3scratch/script.js
+++ projects/d3scratch/script.js
@@ -101,9 +101,10 @@ var data = d3.csv("data/dummyRoomDataUpdate.csv", function(error, data) {
     function showToolTip(d,i){
       tooltip.classed('showit', true)
       tooltip.html('').html('<h2><b>'+d.roomID+'</h2><iframe width=100% height="200" src="https://www.youtube.com/embed/oYZGGDK3kUg?autoplay=1" frameborder="0" allowfullscreen volume="0"></iframe><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.1376991648503!2d-73.99441544876213!3d40.736995479227645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a2868a69e1%3A0x79b19a0b47c6d645!2s6+E+16th+St%2C+New+York%2C+NY+10003!5e0!3m2!1sen!2sus!4v1477595809186" width=100% height="100" frameborder="1" style="border:1" allowfullscreen></iframe><br>Location: '+d.location+'<br>Macs: '+d.macQty+'<br>Windows: '+d.winQty+'<br>Projector: '+d.Projection+'<br>DVD: '+d.DVD+'</b><p><i>This might also include a more verbose description of the room that details the general function, purpose and access to the room, whether that be specialized, designated to specific parties, etc...</i></p><div class="closer"><i class="fa fa-times-circle" fa-lg></div>')
-      .on('mouseleave', hideToolTip)
-      
+      // tooltip.html("<i class='fa fa-times-circle' fa-lg>").on('click',hideToolTip)  
     }
+
+    d3.select('.fa-times-circle').on('click', hideToolTip);
 
     //HIDE IT///////////////////////////////////////
     function hideToolTip(d,i){
