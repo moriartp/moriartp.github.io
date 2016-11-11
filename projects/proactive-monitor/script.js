dc7b4c3f0597b8b404d68552d2464725c9581b2b
System Administrator <root@w-900f-14966.tns.newschool.edu>
2016-11-11 13:32:18 -0500

    cleaning for prod

--- projects/proactive-monitor/script.js
+++ projects/proactive-monitor/script.js
@@ -1,32 +1,15 @@
-// var parseDate = d3.time.format("%d-%b-%y").parse;
 
-var data = d3.csv("proactive-monitor.csv", function(error, data) {
-// var data = d3.csv("dbproactive-monitor.csv", function(error, data) {  
-  console.log(data);
-    // data.forEach(function(d) {
-    //   d.date = d.date = parseDate(d.date);
-    //   // d.close = +d.close;
-    // });   
+var data = d3.csv("db_status/db_status.csv", function(error, data) {  
+  console.log(data);  
 
-  var tiles = d3.select('#container')//('body')
+  var tiles = d3.select('#container')
     .selectAll('.tile')
     .data(data).enter()
     .append('div')
-      .attr('class', function(d) {return "tile "+"server-"+d.server+ " network-"+d.network+ " database"+d.database+ " application"+d.application;})
-      // .attr('id', function(d) {return d.type;})
-      .attr('id', function(d) {return d.service;})
-      // .on('mouseover', function() {
-      //   d3.select(this)
-      //   .transition().duration(100)
-      //   .style('background-color', '#54A5B0')
-      // })
-
-
+      .attr('class', function(d) {return "tile "+"service- "+d.INSTANCE_NAME+ " network-"+d.network+ " database"+d.DATABASE_STATUS+ " application"+d.application;})
+      .attr('id', function(d) {return d.INSTANCE_NAME;})
       .append('text')
-      .html(function (d) { return "<h2>"+d.service+"</h2><br>"; });   
-
-      // d3.selectAll("text").append("mac")
-      // .filter( function(d) {return d.macQty > 0; })
+      .html(function (d) { return "<p class='instanceName'>"+d.INSTANCE_NAME+"</p><br>"; });   
 
       d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"server- "+d.server;})
       .html(function (d) { return "<br><i class='fa fa-server'></i> <br>Server: "+d.server; })
@@ -34,8 +17,8 @@ var data = d3.csv("proactive-monitor.csv", function(error, data) {
       d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"network- "+d.network;})
       .html(function (d) { return "<br><i class='fa fa-connectdevelop'></i><br>Network: "+d.network; })
 
-      d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"database- "+d.database;})
-      .html(function (d) { return "<br><i class='fa fa-database' fa-lg></i><br>Database: "+d.database; })
+      d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"database- "+d.DATABASE_STATUS;})
+      .html(function (d) { return "<br><i class='fa fa-database' fa-lg></i><br>Database: "+d.DATABASE_STATUS; })
 
       d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"application- "+d.application;})
       .html(function (d) { return "<br><i class='fa fa-desktop' fa-lg></i><br>Application: "+d.application; })            
@@ -43,70 +26,20 @@ var data = d3.csv("proactive-monitor.csv", function(error, data) {
   }
 ); 
 
-
-
 ///////////////////////ticker script//////////////////
 var d = new Date();
 var n = (d.getHours()<10?'0':'') + d.getHours();
-// var m = d.getMinutes();
 var m = (d.getMinutes()<10?'0':'') + d.getMinutes();
 var s = (d.getSeconds()<10?'0':'') + d.getSeconds();
 
-
 var lastUpdate = d3.select(".box.fade-in.two")
-      // .append('text').html(function (d) { return "...last update: " + new Date(); })
-      // .append('text').html(function (d) { return "<i>...last update: " + n + ":" + m; }) 
       .append('text').html(function (d) {
-        if (n<12){
-           return "<i>...last update: " + n  + ":" + m +":" + s + " a.m.</i>";
+        if(n==12){
+          return "<i>...last update: " + n  + ":" + m +":" + s + " p.m.</i>";
+        }else if (n<12){
+          return "<i>...last update: " + n  + ":" + m +":" + s + " a.m.</i>";
         } else {
           return "<i>...last update: " + (n - 12) + ":" + m +":" + s + " p.m.</i>";
         }
       ;})
-
-// var inter = setInterval(function() {
-//                 updateData();
-//         }, 3000); 
-
-
-//         // ** Update data section (Called from the onclick)
-// function updateData() {
-//   var data = d3.csv("proactive-monitor-A.csv", function(error, data) {
-//     // data.forEach(function(d) {
-//     // });   
-
-//   var tiles = d3.select('#container')//('body')
-//     .selectAll('.tile')
-//     .data(data).enter()
-//     .append('div')
-//       .attr('class', function(d) {return "tile "+"server-"+d.server+ " network-"+d.network+ " database"+d.database+ " application"+d.application;})
-//       // .attr('id', function(d) {return d.type;})
-//       .attr('id', function(d) {return d.service;})
-//       // .on('mouseover', function() {
-//       //   d3.select(this)
-//       //   .transition().duration(100)
-//       //   .style('background-color', '#54A5B0')
-//       // })
-
-
-//       .append('text')
-//       .html(function (d) { return "<h2>"+d.service+"</h2><br>"; });   
-
-//       // d3.selectAll("text").append("mac")
-//       // .filter( function(d) {return d.macQty > 0; })
-
-//       d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"server- "+d.server;})
-//       .html(function (d) { return "<br><i class='fa fa-server'></i> <br>Server: "+d.server; })
-
-//       d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"network- "+d.network;})
-//       .html(function (d) { return "<br><i class='fa fa-connectdevelop'></i><br>Network: "+d.network; })
-
-//       d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"database- "+d.database;})
-//       .html(function (d) { return "<br><i class='fa fa-database' fa-lg></i><br>Database: "+d.database; })
-
-//       d3.selectAll(".tile").append("div").attr("class",function(d) {return "factor "+"application- "+d.application;})
-//       .html(function (d) { return "<br><i class='fa fa-desktop' fa-lg></i><br>Application: "+d.application; })            
-
-//   }
-// ); 
     
