e3685995c5acfdd521ecdbd74d322ab5af01bc8a
System Administrator <root@w-900f-14966.tns.newschool.edu>
2017-05-30 14:00:56 -0400

    some styling

--- projects/software/js/script.js
+++ projects/software/js/script.js
@@ -15,6 +15,8 @@ function draw(val){
 
   d3.select("body").select("p").text("Input search string: "+Inputdata); 
   // d3.selectAll(".software").style("background-color","green");
+  d3.selectAll(".room").style("display","none");
+  // d3.selectAll("#selection").style("display","none");
 
   d3.selectAll(".software").style("display", function(d) {
     if(d.Software.toLowerCase().indexOf(Inputdata.toLowerCase()) >= 0){
@@ -66,23 +68,39 @@ var data = d3.csv("data/software.csv", function(error, data) {
     //SHOW IT///////////////////////////////////////
     function showToolTip(d,i){
       tooltip.classed('showit', true)
-      tooltip.html('').html('<h2>'+d.Category+' '+d.Software+' '+d.Version+' : CLASSROOMS HERE'+' '+' '+' '+'</h2>')
-      d3.select("#rooms").html("ADD ROOM LIST HERE, PJ")
+      tooltip.html('').html('<h2>'+d.Category+' '+d.Software+' '+d.Version+' '+' '+' '+' '+'</h2>')
+      d3.select("#rooms").html("Available in teh following rooms... ")
 
-      var data = d3.csv("data/supported-spaces.csv", function(error, data) {  
-        data.forEach(function(f) {
-          console.log(d)
+      var data2 = d3.csv("data/supported-spaces.csv", function(error, data2) {  
+        data2.forEach(function(f) {
+          console.log(f)
           // d3.select(#rooms).html("HI");
         }); 
         var room_tiles = d3.select('#rooms')//('body')
           .selectAll('.room')
-          .data(data).enter()
+          .data(data2).enter()
           .append('div')
-            .attr('id', function(d) {return d.RoomNumber;})
-            .attr('class', 'software')
-            .style("display","inline")
+            .attr('id', function(f) {return f.RoomNumber;})
+            .attr('class', 'room')
+            .style("display","inline-block")
+            // .style("border-width","3px")
+            // .style("border-color","gray")
+
+            .on('mouseover', function() {
+                d3.select(this)
+                .transition().duration(100)
+                .style('background-color', 'red')
+                .style('cursor','pointer')
+              })
+              .on('mouseout', function () {
+                d3.select(this)
+                .transition().duration(500)
+                .style('background-color', '#FFF')
+              })
+
+
             .append('text')
-            .html(function (d) { return d.BuildingCode+" "+d.RoomNumber; });
+            .html(function (f) { return f.BuildingCode[0]+f.RoomNumber; });
       });
     
 
