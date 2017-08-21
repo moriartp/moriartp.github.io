8335ae21996593b746daa5e790729ca3d085d8fd
moriartp <moriartp@newschool.edu>
2017-08-21 09:55:46 -0400

    logo, spin button, text resizing

--- projects/slot/sketch.js
+++ projects/slot/sketch.js
@@ -1,3 +1,5 @@
+var img;
+
 var services = [
   "Adobe Creative Cloud",
   "Apple Discounts",
@@ -18,16 +20,16 @@ var blurbs = [
   "The New School provides all degree and credit-seeking students, faculty, and staff \nwith subscriptions for the Adobe Creative Cloud suite.",
   "Visit Apple at The New School to learn more about and to receive special education \npricing on Apple products for the entire university community. ",
   "Free AGI-32 software* available to Parsons Lighting degree-seeking students only. ",
-  "AWS Educate is Amazon's global, grants-based initiative that gives students and educators hands-on experience with AWS technology. ",
+  "AWS Educate is Amazon's global, grants-based initiative that gives students \nand educators hands-on experience with AWS technology. ",
   "Dell is offering discounts of up to 30% on their consumer line products to \nNew School students, faculty, and staff.",
   "New School Students and faculty have access to Mathematica. ",
   "New School Students, faculty and staff have access to Microsoft Office 365. ",
   "Deep discounts on \nRhino/Flamingo/Penguin/Bongo software bundles.",
   "Instructors may apply for an Educator's License.*",
-  "The New School is provides the current version of Symantec AntiVirus Corporate Edition at no cost to New School students, faculty, and staff! ",
-  "The New School has partnered with VMWare to offer our students and faculty a suite of software via the VMWare Academic Program for academic and research use.",
-  "The New School provides all degree and credit-seeking students, faculty, and staff with wireless printing software so that you may enable your personal laptop to print using the print points allocation associated with your NetID.",
-  "The New School provides all degree and credit-seeking students, faculty, and staff with unlimited cloud storage via Google G-Suite."  
+  "The New School is provides the current version of Symantec AntiVirus Corporate Edition\n at no cost to New School students, faculty, and staff! ",
+  "The New School has partnered with VMWare to offer our students and faculty \na suite of software via the VMWare Academic Program for academic and research use.",
+  "The New School provides all degree and credit-seeking students, faculty, and staff \nwith wireless printing software so that you may enable your personal laptop to print.",
+  "The New School provides all degree and credit-seeking students, faculty, and staff \nwith unlimited cloud storage via Google G-Suite."  
 ];
 
 // var brewer = [
@@ -121,6 +123,7 @@ var icon3;
 
 function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
+  img = loadImage("assets/it.png")
   textSize(44);
   textAlign(CENTER, CENTER);
   
@@ -156,6 +159,14 @@ function setup() {
 
 function draw() {
   background(255);
+  image(img, 0, 0, img.width/15, img.height/15);
+
+  textSize(44);
+  fill('red');
+  rect(width*0.4,height*0,width*0.2,height*0.1);
+  fill(255);
+  text("Spin",width/2,height*0.05);
+  // textSize(44);
 
   slot1dur = millis();
   
@@ -221,7 +232,7 @@ function draw() {
     textSize(188);
     text("JACKPOT!", width / 2, height * 0.2);
     fill(0);
-    textSize(44);
+    textSize(24);
 
     text(blurb1, width / 2, height * 0.75);
   } else if ((slot1dur > slotPull + 3500) && (slot1 === slot2 || slot1 === slot3 || slot2 === slot3)) {
@@ -229,7 +240,7 @@ function draw() {
     textSize(111);
     text("WINNER!", width / 2, height * 0.2);
     fill(0);
-    textSize(44);
+    textSize(24);
     if (slot1 === slot2 || slot1 === slot3) {
       text(blurb1, width / 2, height * 0.75);
     } else {
