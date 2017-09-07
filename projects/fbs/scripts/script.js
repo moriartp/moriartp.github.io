44c8559a5aad9d2709e3eda6f8dd18c51b251522
moriartp <moriartp@newschool.edu>
2017-09-06 22:21:33 -0400

    fbs dir added

--- projects/fbs/scripts/script.js
+++ projects/fbs/scripts/script.js
@@ -11,8 +11,11 @@ var data = d3.csv("data/fbs.csv", function(data) {
         .selectAll('.team')
         .data(data).enter()
         .append('div')
-			.attr("class",function(d) { return "team "+d.conference; })
-            .html(function (d) { return d.team+" "+d.nickname+"<br>"+d.conference+" "+d.subdivision; })        
+			.attr("class",function(d) { return "team "+d.subdivision+" "+d.conference; })
+			.style("background-color", function(d) { return d.color1; })
+			.style("color", function(d) { return d.color3; })
+			.style("border", function(d) { return "0.5vw solid "+d.color2; })			
+            .html(function (d) { return "<span class='teamname'>"+d.team+"</span><br><p class='notteamname'>"+d.nickname+"<br>"+d.conference+"<br>"+d.subdivision+"</p>"; })        
 
 
         // d3.selectAll('.team').attr("class",function(d) { return d.conference; })
