4099e9df913be38e734e8fd4368ca9f612d70c8a
moriartp <moriartp@newschool.edu>
2017-09-07 20:02:02 -0400

    new processes elements

--- projects/processes/scripts/script.js
+++ projects/processes/scripts/script.js
@@ -7,92 +7,152 @@ var arr = [];
 
 var data = d3.csv("data/workday_prod_status.csv", function(data) { 
 	data.forEach(function(d){ 
-		d.dateA = new Date(d.dateA); 
+		d.StartTime = new Date(d.StartTime); 
 	});   
 	data.sort(function(a,b){
-            return b.dateA - a.dateA;
+            return b.StartTime - a.StartTime;
     });
+    // console.log(data);
 
-	// DATA LOAD PROCESS //
-    var dataLoad = data.filter(function(d){ return d.process === "Data Load"})
+	// CC-I-Finance-BankOfAmericaStatements_BAI2 //
+    var dataLoad = data.filter(function(d){ return d.ProcessName === "CC-I-Finance-BankOfAmericaStatements_BAI2"})
 	console.log(dataLoad[0]);
 
     var dataLoadChip = d3.select('body')
         .select('#dataLoad')
         .data(dataLoad).enter()
-        d3.select('#dataLoad').attr("class",function(d) { return d.status; })
-            .html(function (d) { return "<h1>"+d.process+"</h1>"+d.status+": "+d.dateA; })
+        d3.select('#dataLoad').attr("class",function(d) { return d.Status; })
+            .html(function (d) { return "<h1>"+d.ProcessName+"</h1>"+d.Status+": "+d.StartTime; })
         
-    // DOCUMENT DELIVERY //
-    var documentDelivery = data.filter(function(e){ return e.process === "Document Delivery"})
+    // CC-I-HCM-Photos //
+    var documentDelivery = data.filter(function(e){ return e.ProcessName === "CC-I-HCM-Photos"})
     console.log(documentDelivery[0]);
 
     var documentDeliveryChip = d3.select('body')
         .select('#documentDelivery')
         .data(documentDelivery).enter()
-        d3.select('#documentDelivery').attr("class",function(e) { return e.status; })
-            .html(function (e) { return "<h1>"+e.process+"</h1>"+e.status +": "+e.dateA; })
+        d3.select('#documentDelivery').attr("class",function(e) { return e.Status; })
+            .html(function (e) { return "<h1>"+e.ProcessName+"</h1>"+e.Status +": "+e.StartTime; })
 
     // DOCUMENT RETRIEVAL //
-    var documentRetrieval = data.filter(function(f){ return f.process === "Document Retrieval"})
+    var documentRetrieval = data.filter(function(f){ return f.ProcessName === "CC-O-Benefits-UnitedHealthcare"})
     console.log(documentRetrieval[0]);
 
     var documentRetrievalChip = d3.select('body')
         .select('#documentRetrieval')
         .data(documentRetrieval).enter()
-        d3.select('#documentRetrieval').attr("class",function(f) { return f.status; })
-            .html(function (f) { return "<h1>"+f.process+"</h1>"+f.status+": "+f.dateA; })
+        d3.select('#documentRetrieval').attr("class",function(f) { return f.Status; })
+            .html(function (f) { return "<h1>"+f.ProcessName+"</h1>"+f.Status+": "+f.StartTime; })
 
-    // IMPORT ERROR AGGREGATION //
-    var importErrorAggregation = data.filter(function(g){ return g.process === "Import Error Aggregation"})
+    // CC-O-Finance-ACH-PPD//
+    var importErrorAggregation = data.filter(function(g){ return g.ProcessName === "CC-O-Finance-ACH-PPD"})
     console.log(importErrorAggregation[0]);
 
     var importErrorAggregation = d3.select('body')
         .select('#importErrorAggregation')
         .data(importErrorAggregation).enter()
-        d3.select('#importErrorAggregation').attr("class",function(g) { return g.status; })
-            .html(function (g) { return "<h1>"+g.process+"</h1>"+g.status+": "+g.dateA; })
+        d3.select('#importErrorAggregation').attr("class",function(g) { return g.Status; })
+            .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
 
-    // printServiceRetrieveReportAndParmsAndGeneratePDF //
-    var printServiceRetrieveReportAndParmsAndGeneratePDF = data.filter(function(g){ return g.process === "PrintServiceRetrieveReportAndParmsAndGeneratePDF"})
+    // CC-O-Finance-PaymodeX-Extract //
+    var printServiceRetrieveReportAndParmsAndGeneratePDF = data.filter(function(g){ return g.ProcessName === "CC-O-Finance-PaymodeX-Extract"})
     console.log(printServiceRetrieveReportAndParmsAndGeneratePDF[0]);
 
     var printServiceRetrieveReportAndParmsAndGeneratePDFChip = d3.select('body')
         .select('#printServiceRetrieveReportAndParmsAndGeneratePDF')
         .data(printServiceRetrieveReportAndParmsAndGeneratePDF).enter()
-        d3.select('#printServiceRetrieveReportAndParmsAndGeneratePDF').attr("class",function(g) { return g.status; })
-            .html(function (g) { return "<h1>"+g.process+"</h1>"+g.status+": "+g.dateA; })
+        d3.select('#printServiceRetrieveReportAndParmsAndGeneratePDF').attr("class",function(g) { return g.Status; })
+            .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
 
-    // RETRIEVAL //
-    var retrieval = data.filter(function(g){ return g.process === "Retrieval"})
+    // CC-O-Finance-PaymodeX-Transport//
+    var retrieval = data.filter(function(g){ return g.ProcessName === "CC-O-Finance-PaymodeX-Transport"})
     console.log(retrieval[0]);
 
     var retrieval = d3.select('body')
         .select('#retrieval')
         .data(retrieval).enter()
-        d3.select('#retrieval').attr("class",function(g) { return g.status; })
-            .html(function (g) { return "<h1>"+g.process+"</h1>"+g.status+": "+g.dateA; })
+        d3.select('#retrieval').attr("class",function(g) { return g.Status; })
+            .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
 
-    // SCHEDULE LARGE REPORTS //
-    var scheduleLargeReports = data.filter(function(g){ return g.process === "Schedule Large Reports"})
-    console.log(scheduleLargeReports[0]);
+    // // SCHEDULE LARGE REPORTS //
+    // var scheduleLargeReports = data.filter(function(g){ return g.process === "Schedule Large Reports"})
+    // console.log(scheduleLargeReports[0]);
 
-    var scheduleLargeReports = d3.select('body')
-        .select('#scheduleLargeReports')
-        .data(scheduleLargeReports).enter()
-        d3.select('#scheduleLargeReports').attr("class",function(g) { return g.status; })
-            .html(function (g) { return "<h1>"+g.process+"</h1>"+g.status+": "+g.dateA; })
+    // var scheduleLargeReports = d3.select('body')
+    //     .select('#scheduleLargeReports')
+    //     .data(scheduleLargeReports).enter()
+    //     d3.select('#scheduleLargeReports').attr("class",function(g) { return g.status; })
+    //         .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
 
-    // TRANSFORMATION //
-    var transformation = data.filter(function(g){ return g.process === "Transformation"})
-    console.log(transformation[0]);
+    // XXX //
+    var CCOPayrollADPTaxFilingPeriodic = data.filter(function(g){ return g.ProcessName === "CC-O-Payroll-ADP-Tax-Filing-Periodic"})
+    console.log(CCOPayrollADPTaxFilingPeriodic[0]);
 
-    var transformation = d3.select('body')
-        .select('#transformation')
-        .data(transformation).enter()
-        d3.select('#transformation').attr("class",function(g) { return g.status; })
-            .html(function (g) { return '<h1>'+g.process+"</h1>"+g.status+": "+g.dateA; })
+    var CCOPayrollADPTaxFilingPeriodicChip = d3.select('body')
+        .select('#CCOPayrollADPTaxFilingPeriodic')
+        .data(CCOPayrollADPTaxFilingPeriodic).enter()
+        d3.select('#CCOPayrollADPTaxFilingPeriodic').attr("class",function(g) { return g.Status; })
+            .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
 
+    // CC-O-Payroll-ADP-WagePayments //
+    var CCOPayrollADPWagePayments = data.filter(function(g){ return g.ProcessName === "CC-O-Payroll-ADP-WagePayments"})
+    console.log(CCOPayrollADPWagePayments[0]);
+
+    var CCOPayrollADPWagePaymentsChip = d3.select('body')
+        .select('#CCOPayrollADPWagePayments')
+        .data(CCOPayrollADPWagePayments).enter()
+        d3.select('#CCOPayrollADPWagePayments').attr("class",function(g) { return g.Status; })
+            .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
+
+    // // XXX //
+    // var XXX = data.filter(function(g){ return g.ProcessName === "XXX})
+    // console.log(transformation[0]);
+
+    // var XXXChip = d3.select('body')
+    //     .select('#XXX')
+    //     .data(XXX).enter()
+    //     d3.select('#XXX').attr("class",function(g) { return g.Status; })
+    //         .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
+
+    // // XXX //
+    // var XXX = data.filter(function(g){ return g.ProcessName === "XXX})
+    // console.log(transformation[0]);
+
+    // var XXXChip = d3.select('body')
+    //     .select('#XXX')
+    //     .data(XXX).enter()
+    //     d3.select('#XXX').attr("class",function(g) { return g.Status; })
+    //         .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
+
+    // // XXX //
+    // var XXX = data.filter(function(g){ return g.ProcessName === "XXX})
+    // console.log(transformation[0]);
+
+    // var XXXChip = d3.select('body')
+    //     .select('#XXX')
+    //     .data(XXX).enter()
+    //     d3.select('#XXX').attr("class",function(g) { return g.Status; })
+    //         .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
+
+    // // XXX //
+    // var XXX = data.filter(function(g){ return g.ProcessName === "XXX})
+    // console.log(transformation[0]);
+
+    // var XXXChip = d3.select('body')
+    //     .select('#XXX')
+    //     .data(XXX).enter()
+    //     d3.select('#XXX').attr("class",function(g) { return g.Status; })
+    //         .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
+
+    // // XXX //
+    // var XXX = data.filter(function(g){ return g.ProcessName === "XXX})
+    // console.log(transformation[0]);
+
+    // var XXXChip = d3.select('body')
+    //     .select('#XXX')
+    //     .data(XXX).enter()
+    //     d3.select('#XXX').attr("class",function(g) { return g.Status; })
+    //         .html(function (g) { return "<h1>"+g.ProcessName+"</h1>"+g.Status+": "+g.StartTime; })
 
 
 // //////////////////////////////////////////////////////////////////////////////
