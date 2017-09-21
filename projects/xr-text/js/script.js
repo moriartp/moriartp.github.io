056cafb4a8c905006812bb6de252c3d4572531b6
moriartp <moriartp@newschool.edu>
2017-09-21 16:51:37 -0400

    slides

---
+++ projects/xr-text/js/script.js
@@ -0,0 +1,41 @@
+
+d3.csv('assets/inventory.csv', function (error,data) {
+  console.log(data);
+
+  function tabulate(data, columns) {
+        var table = d3.select('#table-container').append('table')
+        var thead = table.append('thead')
+        var tbody = table.append('tbody');
+
+        // append the header row
+        thead.append('tr')
+          .selectAll('th')
+          .data(columns).enter()
+          .append('th')
+            .text(function (column) { return column; });
+
+        // create a row for each object in the data
+        var rows = tbody.selectAll('tr')
+          .data(data)
+          .enter()
+          .append('tr')
+          .attr("class","rowA");
+
+        // create a cell in each row for each column
+        var cells = rows.selectAll('td')
+          .data(function (row) {
+            return columns.map(function (column) {
+              return {column: column, value: row[column]};
+            });
+          })
+          .enter()
+          .append('td')
+            .text(function (d) { return d.value; });
+
+      return table;
+    }
+
+    // render the table(s)
+    tabulate(data, ['Type','Name', 'Description']); // 2 column table
+
+});
