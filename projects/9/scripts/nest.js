fa8f25e7b69da36c92db7918529eb77b130fc043
moriartp <moriartp@newschool.edu>
2017-10-12 18:34:58 -0400

    api play

--- projects/9/scripts/nest.js
+++ projects/9/scripts/nest.js
@@ -1,18 +1,18 @@
 var api = "https://projects.newschool.edu/psa/api.do?function=query&table=db_task&project_id=593&token=fa71d5fc-1b40-4512-9eb6-a7b0f8ec828e&data-format=json";
-
+// var obj = [];
 var data = d3.json(api, function(data) { 
 	// console.log(data.rows[0][0].n);
 	// console.log(data.rows[0][0].v);
 	// console.log(data.rows[0]);
 	data = data.rows;
-	console.log(data);
+	// console.log(data);
 
 	// console.log(JSON.stringify(data));
 
 
 
 	var str = JSON.stringify(data);
-	console.log(str);
+	// console.log(str);
 
 	str = str.replace(/"n":"/g,'"');
 	str = str.replace(/,"v":/g,':');
@@ -26,22 +26,12 @@ var data = d3.json(api, function(data) {
 	console.log(str);
 
 	var obj = JSON.parse(str);
-	console.log(obj[0].id);
+	console.log(obj);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	// var dataset = d3.json(obj, function(data) {  
+	// 	console.log(dataset);  
+	// 	console.log('hello');  
+	// });
 
 
 });
