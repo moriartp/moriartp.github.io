2e57e2a9337f4bcabd72d15f832d362f5fcaefba
System Administrator <root@w-900f-14966.tns.newschool.edu>
2017-10-26 09:40:11 -0400

    new neue font style

--- projects/xrtest/js/script.js
+++ projects/xrtest/js/script.js
@@ -84,3 +84,53 @@ d3.csv('assets/software.csv', function (error,data) {
     tabulate(data, ['Type','Name', 'Description']); // 2 column table
 
 });
+
+// COURSES TABLE
+d3.csv('assets/xrcourses.csv', function (error,data) {
+  console.log(data);
+
+  function tabulate(data, columns) {
+        var table = d3.select('#table-container3').append('table')
+        var thead = table.append('thead')
+        var tbody = table.append('tbody');
+
+        // append the header row
+        thead.append('tr')
+          .selectAll('th')
+          .data(columns).enter()
+          .append('th')
+            .text(function (column) { return column; });
+
+        // create a row for each object in the data
+        var rows = tbody.selectAll('tr')
+          .data(data)
+          .enter()
+          .append('tr')
+          .attr("class","row-class");
+
+        // create a cell in each row for each column
+        var cells = rows.selectAll('td')
+          .data(function (row) {
+            return columns.map(function (column) {
+              return {column: column, value: row[column]};
+            });
+          })
+          .enter()
+          .append('td')
+            .text(function (d) { return d.value; });
+            // .html(function (d) { return '<a href="'+d.courseURL+'">'+d.value; });
+
+      return table;
+    }
+
+    // render the table(s)
+    tabulate(data, ['Name','ID', 'School', 'Description']); // 4 column table
+
+});
+
+
+
+
+
+
+
